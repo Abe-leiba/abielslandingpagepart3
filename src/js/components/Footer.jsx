0a355a4cebab957195a534147df02ce17d3bb19a
Abe-leiba <abel_con@yahoo.com>
2025-09-23 00:11:44 +0000

    landing page 3rd times a charm

---
+++ src/js/components/Footer.jsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+
+    return (
+        <>
+
+  <footer>
+    <p>&copy; 2025 My Website. All rights reserved.</p>
+    <p>Contact us: <a href="mailto:info@mywebsite.com">info@mywebsite.com</a></p>
+  </footer>
+
+        </>
+
+    );
+}
+
+export default Footer;
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,36 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Cardrow from "./Cardrow";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-            
+		<>
+		<Navbar/>
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="row">
+			<div className="col-1"></div>
+			<div className="col-10">
+				<Hero/>
+			</div>
+			<div className="col-1"></div>
 		</div>
+
+		<div className="row">
+			<div classname="col-1"></div>
+			<div className="col-10">
+				<Cardrow/>
+			</div>
+			<div classname="col-1"></div>
+		</div>
+		
+		
+			<Footer/>
+
+			
+
+		
+		</>
 	);
 };
 
